1027c252850d363c8e028f7486928db7da97a63c
ebube911 <82494431+ebube911@users.noreply.github.com>
2021-06-27 15:27:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    getVisitCount();
+});
+
+const apiGateway = 'https://gkt25sgaaa.execute-api.us-east-2.amazonaws.com/Prod/visitor/'; 
+
+const getVisitCount = () => {
+    let count = 0;
+    fetch(apiGateway, {
+        mode: 'cors',
+    })
+    .then(response => {
+        return response.json()
+    })
+    .then(res => {
+        const count = res.Attributes.visitcount;
+        document.getElementById('counter').innerText =count;
+        document.getElementById('visitorElem').style.display = 'block';
+    })
+    return count;
+}
+
+
+
